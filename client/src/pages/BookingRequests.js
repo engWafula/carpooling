2e2934260b510ce9ad616cb6a593faff5251f0c0
engWafula <wafulaallan5@gmail.com>
2024-07-03 09:06:58 +0300

    adding fix

--- client/src/pages/BookingRequests.js
+++ client/src/pages/BookingRequests.js
@@ -5,45 +5,49 @@ import { getAllBookingRequests, getAllBookings } from "../redux/actions/bookingA
 import { Col, Row } from "antd";
 import Spinner from '../components/Spinner';
 import moment from "moment";
+
 function BookingRequests() {
   const dispatch = useDispatch();
   const { bookings } = useSelector((state) => state.bookingsReducer);
   const {loading} = useSelector((state) => state.alertsReducer);
   const user = JSON.parse(localStorage.getItem("user"));
+
   useEffect(() => {
     dispatch(getAllBookingRequests());
-  }, []);
-    console.log(bookings,"AM THE BOOKINGSKKSJSJS")
+  }, [dispatch]);
+
+  const userBookings = bookings?.filter(o => o?.car?.owner === user._id) || [];
+
   return (
     <DefaultLayout>
-        {loading && (<Spinner />)}
+      {loading && <Spinner />}
       <h3 className="text-center mt-2">My Booking Requests</h3>
-    
+
       <Row justify="center" gutter={16}>
         <Col lg={16} sm={24}>
-         
-            {bookings.filter(o=>o.car.owner==user._id).map((booking) => {
-             return <Row gutter={16} className="bs1 mt-3 text-left">
+          {userBookings.length > 0 ? (
+            userBookings.map((booking) => (
+              <Row gutter={16} className="bs1 mt-3 text-left" key={booking._id}>
                 <Col lg={6} sm={24}>
-                    <p><b>Customer Email {booking.user.username}</b></p>
-                    <p>Total hours : <b>{booking.totalHours}</b></p>
-                    <p>Rent per hour : <b>{booking.car.rentPerHour}</b></p>
-                    <p>Total amount : <b>{booking.totalAmount}</b></p>
+                  <p><b>Customer Email: {booking?.user?.username}</b></p>
+                  <p>Total hours: <b>{booking?.totalHours}</b></p>
+                  <p>Rent per hour: <b>{booking?.car?.rentPerHour}</b></p>
+                  <p>Total amount: <b>{booking.totalAmount}</b></p>
                 </Col>
-
                 <Col lg={12} sm={24}>
-                <p>Transaction Id : <b>{booking.transactionId}</b></p>
-                <p>From: <b>{booking.bookedTimeSlots.from}</b></p>
-                <p>To: <b>{booking.bookedTimeSlots.to}</b></p>
-                <p>Date of booking: <b>{moment(booking.createdAt).format('MMM DD yyyy')}</b></p>
+                  <p>Transaction Id: <b>{booking.transactionId}</b></p>
+                  <p>From: <b>{booking.bookedTimeSlots.from}</b></p>
+                  <p>To: <b>{booking.bookedTimeSlots.to}</b></p>
+                  <p>Date of booking: <b>{moment(booking.createdAt).format('MMM DD yyyy')}</b></p>
                 </Col>
-
                 <Col lg={6} sm={24} className='text-right'>
-                    <img style={{borderRadius:5}} src={booking.car.image}  height="140" className="p-2"/>
+                  <img style={{borderRadius: 5}} src={booking.car.image} height="140" className="p-2" alt="Car" />
                 </Col>
-              </Row>;
-            })}
-          
+              </Row>
+            ))
+          ) : (
+            <h6 className="text-center mt-4">No Requests Available</h6>
+          )}
         </Col>
       </Row>
     </DefaultLayout>
